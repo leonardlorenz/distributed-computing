4414f5badb3f063148e5861589c0b3d4594bcc30
Leonard Lorenz <contact@metr1xx.de>
2018-10-12 12:35:23 +0200

    program to print hostname implemented

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,3 +1,9 @@
-void main(int argc, char** argv){
-    cout << gethostname() << endl;
+#include <iostream>
+#include <unistd.h>
+#include <sys/param.h>
+
+int main(int argc, char** argv){
+    char name[MAXHOSTNAMELEN];
+    gethostname(name, sizeof name);
+    std::cout << name << std::endl;
 }
