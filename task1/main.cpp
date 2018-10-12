29d0c29180bad2ca577f6bf7cb2ffe055adaeee4
Leonard Lorenz <contact@metr1xx.de>
2018-10-12 13:39:31 +0200

    added documentation

--- task1/main.cpp
+++ task1/main.cpp
@@ -2,6 +2,14 @@
 #include <unistd.h>
 #include <sys/param.h>
 
+/**
+ * char name[]:
+ *      a character array that stores the hostname
+ * int MAXHOSTNAMELEN:
+ *      constant provided by param.h, size of the hostname char[]
+ * int gethostname(char[] hostname, sizeof hostname):
+ *      writes the hostname to the char[] that gets parsed over
+ **/
 int main(int argc, char** argv){
     char name[MAXHOSTNAMELEN];
     gethostname(name, sizeof name);
