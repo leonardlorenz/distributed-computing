f1a94fa3d99061be34c16f8f22320d02b86d76b9
Leonard Lorenz <contact@metr1xx.de>
2018-11-23 15:14:35 +0100

    it werks

--- task5/file_receiver.py
+++ task5/file_receiver.py
@@ -6,24 +6,25 @@ import base64
 class file_receiver(Thread):
     def __init__(self, CONN, filename):
         self.CONN = CONN
+        self.filename = filename
         Thread.__init__(self)
 
     def run(self):
         while main.STILL_RUNS:
             try:
                 all_data = []
-                while True:
-                    data = self.CONN.recv(8192)
-                    if not data: break
-                    all_data.append(data)
-                all_data_str = ''.join(all_data)
-                message = all_data_str.decode('utf-8')
-                msg_arr = message.split("\r\n")
+                data = bytearray(8192)
+                self.CONN.recv_into(data)
+                all_data.append(data.decode("utf-8"))
+                for i in range(0, len(all_data)):
+                    all_data_str = "".join(all_data[i])
+                all_data_str
+                msg_arr = all_data_str.split("\r\n")
                 if msg_arr[0] == "dslp/1.2":
                     if msg_arr[1] == "peer notify":
                         # ignore line 2
                         print(msg_arr[3])
-                        file_to_write = open(filename, w)
-                        file_to_write.write(base64.b64decode(msg_arr[4]))
+                        file_to_write = open(self.filename, "w")
+                        file_to_write.write(base64.b64decode(msg_arr[4]).decode("utf-8"))
             except socket.timeout:
                 continue
--- task5/file_sender.py
+++ task5/file_sender.py
@@ -11,17 +11,16 @@ class file_sender(Thread):
         Thread.__init__(self)
 
     def run(self):
-        LINE_END = main.LINE_END
-        try:
-            file_to_send = open(self.filename, "r")
-            text = file_to_send.read()
-            file_as_text = base64.b64encode(text.encode('utf-8'))
-            user_input = input()
-            message = "dslp/1.2" + LINE_END + "peer notify" + LINE_END + self.peer + LINE_END + user_input + LINE_END + file_as_text.decode("utf-8") + LINE_END + "dslp/end" + LINE_END
-            print("message created")
-            self.CONN.sendall(message.encode('utf-8'))
-            print("message sent")
-        except IOError:
-            print("Could not open " + self.filename + ". Are you sure you have read permissions on this file?")
-            main.STILL_RUNS = False
-
+        while True:
+            LINE_END = main.LINE_END
+            try:
+                file_to_send = open(self.filename, "r")
+                text = file_to_send.read()
+                file_as_text = base64.b64encode(text.encode('utf-8'))
+                user_input = "hello world!"
+                message = "dslp/1.2" + LINE_END + "peer notify" + LINE_END + self.peer + LINE_END + user_input + LINE_END + file_as_text.decode("utf-8") + LINE_END + "dslp/end" + LINE_END
+                self.CONN.sendall(message.encode('utf-8'))
+                print("message sent")
+                time.sleep(5)
+            except IOError:
+                print("Could not open " + self.filename + ". Are you sure you have read permissions on this file?")
