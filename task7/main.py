db92dfa45565783b130efe3f0012a7d1d90e950d
Leonard Lorenz <contact@metr1xx.de>
2018-11-30 15:48:54 +0100

    implemented response_time() and error()

--- task7/main.py
+++ task7/main.py
@@ -1,18 +1,66 @@
 import socket
+import threading
+import time
 
 HOST = ""
-PORT = "44444"
+PORT = 44444
+IS_RUNNING = True
 
 def main():
+    client_response_time(None, None)
     SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-	s.bind((HOST, PORT))
+        SOCK.bind((HOST, PORT))
     except socket.error as msg:
-	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()
+        print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+        sys.exit()
     SOCK.listen(10)
-    while True:
-        SOCK.accept()
+    while IS_RUNNING:
+        conn, addr = SOCK.accept()
+        print("Connected to " + addr[0] + ":" + str(addr[1]))
+        threading.Thread(None, listen_for_messages, None, conn, addr)
+
+def listen_for_messages(conn, addr):
+    while IS_RUNNING:
+        all_data = []
+        data = bytearray(8192)
+        conn.recv_into(data)
+        all_data.append(data)
+        all_data_str = ""
+        for i in range(0, len(all_data)):
+            all_data_str = all_data_str + all_data[i]
+        msg_arr = all_data_str.split("\r\n")
+        if msg_arr[0] == "dslp/1.2":
+            if msg_arr[1] == "request time":
+                client_response_time(conn, addr)
+            if msg_arr[1] == "group join":
+                client_group_join(conn, addr)
+            if msg_arr[1] == "group leave":
+                client_group_leave(conn, addr)
+            if msg_arr[1] == "group notify":
+                client_group_notify(conn, addr)
+            if msg_arr[1] == "peer notify":
+                client_peer_notify(conn, addr)
+
+def client_response_time(conn, addr):
+    date_time = time.time()
+    # yyyy-MM-dd'T'HH:mm:ssX
+    date_str = time.strftime("%Y-%m-%dT%H:%M:%S%Z", time.gmtime(date_time))
+    message = "dslp/1.2\r\n" + "response time\r\n" + date_str + "\r\n" + "dslp/end\r\n"
+    conn.sendall(message.encode("utf-8"))
+
+def client_group_join(conn, addr):
+    print("")
+
+def client_group_leave(conn, addr):
+    print("")
+
+def client_group_notify(conn, addr):
+    print("")
+
+def send_error(conn, addr, error):
+    message = "dslp/1.2\r\n" + "error\r\n" + str(error) + "\r\n" + "dslp/end\r\n"
+    conn.sendall(message.encode("utf-8"))
 
 if __name__ == "__main__":
     main()
